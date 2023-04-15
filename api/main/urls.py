a978949065e494ab86c1f60d1d217db44f292448
elamirkad <elamirkad@gmail.com>
2023-04-15 16:36:13 +0600

    supplies

--- api/main/urls.py
+++ api/main/urls.py
@@ -1,9 +1,10 @@
 
 from django.urls import path, include
 from rest_framework import routers
-from .views import SalesViewSet
+from .views import SalesViewSet, SupplyViewSet
 
 router = routers.DefaultRouter()
+router.register(r'supplies', SupplyViewSet)
 router.register(r'sales', SalesViewSet)
 
 urlpatterns = [
