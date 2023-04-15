e4ce1f01f06612ee87f8d187619c8abf0ab4e1e3
elamirkad <elamirkad@gmail.com>
2023-04-15 16:10:39 +0600

    django models

--- api/main/models.py
+++ api/main/models.py
@@ -1,21 +1,16 @@
 from django.db import models
 
-class Product(models.Model):
-    user_id = models.IntegerField(primary_key=True)
-    product_id = models.IntegerField(primary_key=True)
-    price_id = models.IntegerField(null=True)
 
-class Zakupki(models.Model):
-    user_id = models.IntegerField(primary_key=True)
-    price_id = models.IntegerField(primary_key=True)
-    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+class Supply(models.Model):
+    barcode = models.CharField(max_length=255)
+    quantity = models.IntegerField()
     datetime = models.DateTimeField()
+    price = models.FloatField()
     sold_amount = models.FloatField()
-    bought_amount = models.FloatField()
 
-class Sales(models.Model):
-    user_id = models.IntegerField(primary_key=True)
-    price_id = models.IntegerField(primary_key=True)
-    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+class Sale(models.Model):
+    barcode = models.CharField(max_length=255)
+    quantity = models.IntegerField()
     datetime = models.DateTimeField()
-    amount_not_connected = models.FloatField()
+    price = models.FloatField()
